935b7748343aa0535faf9f11b9f72c56fb077252
mila811230 <bluegalaxy1230@gmail.com>
2024-09-27 10:31:59 +0900

    프로젝트 초기화

--- src/main/java/com/mysite/sbb/HelloController.java
+++ src/main/java/com/mysite/sbb/HelloController.java
@@ -1,21 +1,15 @@
 package com.mysite.sbb;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Controller; 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
-    @GetMapping("/hello")
-//    @ResponseBody
+    @GetMapping("/hello")   //요청이 왓을때
+   @ResponseBody     //텍스트메시지를 보내는거   뿌려주는거
     public String hello() {
-        return "hello";
+        return "Hello World";
     }
-    
-    @GetMapping("/event")
-    public String event() {
-    	String filename = "myevent";
-    	
-    	return filename;
-    }
-}
+   
+  }
