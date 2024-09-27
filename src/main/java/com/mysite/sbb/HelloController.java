37f7a0f4b5d914223ef7c58c06432ce39831f350
casey <casey@pc01>
2024-09-27 21:28:36 +0900

    노트북 GIT 연결

--- src/main/java/com/mysite/sbb/HelloController.java
+++ src/main/java/com/mysite/sbb/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloController {
     @GetMapping("/hello")   //요청이 왓을때
-   @ResponseBody     //텍스트메시지를 보내는거   뿌려주는거
+   @ResponseBody     //텍스트메시지를 보내는거   뿌려주는거111
     public String hello() {
         return "Hello World";
     }
